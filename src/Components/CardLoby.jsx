1d3f477c27607e2d8703bf4b50393fd7d634219e
NANDA <NANDA@gmail.com>
2023-12-12 03:46:01 -0800

    first cuy

--- src/Components/CardLoby.jsx
+++ src/Components/CardLoby.jsx
@@ -1,10 +1,10 @@
 const CardLoby = (props) => {
   return (
-    <div className=" w-80 h-40 md:w-[900px] md:h-96 xl:w-[1000px] bg-white drop-shadow-lg rounded-xl font-bold">
+    <div className=" w-80 h-40 md:w-[650px] md:h-96 xl:w-[800px] bg-white drop-shadow-lg rounded-xl font-bold">
       <div className=" p-1 ml-2">
         <img src={`${props.img}`} className=" float-left h-40 md:h-96"></img>
-        <p className=" mt-8 text-lg md:text-4xl xl:text-7xl">{`${props.text}`}</p>
-        <p className="  mt-1 text-lg md:text-2xl xl:text-7xl">{`${props.text2}`}</p>
+        <p className=" mt-8 text-lg md:text-5xl  xl:text-7xl p-1">{`${props.text}`}</p>
+        <p className="  mt-1 text-lg md:text-4xl xl:text-7xl p-1">{`${props.text2}`}</p>
 
       </div>
     </div>
--- src/Components/Fitur/NightBrightMode.jsx
+++ src/Components/Fitur/NightBrightMode.jsx
@@ -1,7 +1,7 @@
 const NightBrightMode = () => {
     return (
       <button>
-        <img src="sleep.png" className="h-5 md:h-8" alt="NightBright Mode" />
+        <img src="sleep.png" className="h-7 md:h-8" alt="NightBright Mode" />
       </button>
     );
   };
