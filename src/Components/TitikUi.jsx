b506649713116a5cf2d9cde2bfd9dd8f44f89b4a
NANDA <NANDA@gmail.com>
2023-12-12 00:50:10 -0800

    first lagi

--- src/Components/TitikUi.jsx
+++ src/Components/TitikUi.jsx
@@ -1,11 +1,11 @@
 const TitikUi = () => {
   return (
     <div className=" flex space-x-2" >
-        <div className=" bg-red-600 w-3 h-3 rounded-full">
+        <div className=" bg-red-600 w-2 h-2 rounded-full md:w-3 md:h-3">
         </div>
-        <div className=" bg-yellow-400 w-3 h-3 rounded-full">
+        <div className=" bg-yellow-400 w-2 h-2 rounded-full md:w-3 md:h-3">
         </div>
-        <div className=" bg-green-500 w-3 h-3 rounded-full">
+        <div className=" bg-green-500 w-2 h-2 rounded-full md:w-3 md:h-3">
         </div>
     </div>
   )
